e0d1b0f2d060ac69d7a14891d60d18a4ebf73822
Aditya Pradita <pradita@rollingglory.com>
2018-09-10 20:05:41 +0700

    + starbase-router

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -1,10 +1,5 @@
 <template>
 <div id="app">
-  <h3>Starbase Router</h3>
-  <h4>
-    <router-link class="link" to="/data/people">Meet the Characters</router-link> |
-    <router-link class="link" to="/data/planets">Explore the Planets</router-link>
-  </h4>
-  <router-view></router-view>
+    <h3>Starbase Router</h3>
 </div>
 </template>
